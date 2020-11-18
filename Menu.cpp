43f6c638773f7450637fec4829ddfce805853b55
Pierre-Loup Martin <lelabodutroisieme@gmail.com>
2020-11-18 20:34:53 +0100

    Stopping work on the idea of independant items only linked by their lists. To much work to keep eveyrthing up to date, need seomething simpler. Will move this to a dedicated branch.

--- Menu.cpp
+++ Menu.cpp
@@ -1,3 +1,21 @@
+/*
+ * This Arduino library is for managing a menu system
+ * Copyright (C) 2020  Pierre-Loup Martin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "Menu.h"
 
 // MenuItem
@@ -7,21 +25,26 @@ MenuItem::MenuItem(){
 	_parents = NULL;
 	_caller = NULL;
 	_parentsSize = 0;
+	_hasFocus = 0;
 }
 
 MenuItem::~MenuItem(){
+	for(uint16_t i = 0; i < _parentsSize; ++i){
+		_parents[i]->removeChild(this);
+	}
+
 	free(_name);
 	free(_parents);
 	free(_caller);
 }
 
 void MenuItem::name(const char *name){
-	char *tmp = (char*)realloc(_name, sizeof(name));
+	char *tmp = (char*)realloc(_name, strlen(name));
 	if(!tmp){
 		return;
 	}
 	_name = tmp;
-	*_name = *name;
+	strcpy(_name, name);
 }
 
 const char* MenuItem::name() const{
@@ -39,12 +62,52 @@ bool MenuItem::hasParent(MenuItem *parent){
 bool MenuItem::hasParents(){
 	return (bool)_parentsSize;
 }
+/*
+MenuItem* MenuItem::moveFocus(int8_t focus = 0){
+	if(focus > 0){
+		if(_focus >= (_childrenSize - 1)) return this;
+		_focus++;		
+	} else if(focus < 0){
+		if(_focus == 0) return this;
+		_focus--;		
+	}
+
+	return _children[_focus];
+}
+*/
+
+bool MenuItem::hasFocus(){
+	if(!hasParents() || _caller == NULL) return false;
+	if(_caller->getFocus() == this) return true;
+	return false;
+}
+
+MenuItem* MenuItem::focusNextItem(){
+	if(!hasParents() || _caller == NULL) return NULL;
+	return _caller->focusNextItem();
+}
+
+MenuItem* MenuItem::focusPreviousItem(){
+	if(!hasParents() || _caller == NULL) return NULL;
+	return _caller->focusPreviousItem();
+}
+
+MenuItem* MenuItem::getNextItem(){
+	if(!hasParents() || _caller == NULL) return NULL;
+	MenuItem *next = (MenuItem*)(_caller->setNext());
+	return next;
+}
+
+MenuItem* MenuItem::getPreviousItem(){
+	if(!hasParents() || _caller == NULL) return NULL;
+	return _caller->getPrevious();
+}
 
 
 // Protected
-MenuItem* MenuItem::addParent(MenuItem *parent){
+MenuItem* MenuItem::addParent(MenuList *parent){
 	_parentsSize++;
-	MenuItem **tmp = (MenuItem**)realloc(_parents, sizeof(MenuItem*) * _parentsSize);
+	MenuList **tmp = (MenuList**)realloc(_parents, sizeof(MenuList*) * _parentsSize);
 	if(!tmp){
 		_parentsSize--;
 		return NULL;
@@ -55,7 +118,7 @@ MenuItem* MenuItem::addParent(MenuItem *parent){
 	return parent;
 }
 
-MenuItem* MenuItem::removeParent(MenuItem *parent){
+MenuItem* MenuItem::removeParent(MenuList *parent){
 	if(_parentsSize == 0) return NULL;
 	_parentsSize--;
 
@@ -64,7 +127,7 @@ MenuItem* MenuItem::removeParent(MenuItem *parent){
 		return parent;
 	}
 
-	MenuItem **tmp = (MenuItem**)malloc(sizeof(MenuItem*) * _parentsSize);
+	MenuList **tmp = (MenuList**)malloc(sizeof(MenuList*) * _parentsSize);
 	if(!tmp){
 		_parentsSize++;
 		return NULL;
@@ -88,9 +151,13 @@ MenuList::MenuList(){
 	_children = NULL;
 	_childrenSize = 0;
 	_focus = 0;
+	_index = 0;
 }
 
 MenuList::~MenuList(){
+	for(uint16_t i = 0; i < _childrenSize; ++i){
+		_children[i]->removeParent(this);
+	}
 	free(_children);
 }
 
@@ -114,6 +181,8 @@ MenuItem* MenuList::removeChild(MenuItem * child){
 	if(_childrenSize == 0) return NULL;
 	if(!hasChild(child)) return NULL;
 	_childrenSize--;
+
+	if(!(child->removeParent(this))) return NULL;
 
 	if(_childrenSize == 0){
 		delete(_children);
@@ -140,26 +209,71 @@ MenuItem* MenuList::removeChild(MenuItem * child){
 
 MenuItem* MenuList::deleteChild(MenuItem * child){
 	if(!removeChild(child)) return NULL;
-	if(!(child->removeParent(this))) return NULL;
 	if(!(child->hasParents())) delete child;
-	return child;
+	return this;
 }
 
-MenuItem* MenuList::focus(){
+MenuItem* MenuList::getFocus(){
+	if(!hasChildren()) return NULL;
 	return _children[_focus];
 }
 
-MenuItem* MenuList::previous(){
-	if(_focus == 0) return this;
-	_focus--;
-	return focus();
+MenuItem* MenuList::focusNextItem(){
+	if(!hasChildren()) return NULL;
+	if(_index < (_childrenSize - 1)) return _children[++_focus];
+	return NULL;
 }
 
-MenuItem* MenuList::next(){
-	if(_focus >= _childrenSize) return this;
-	_focus++;
-	return focus();
+MenuItem* MenuList::focusPreviousItem(){
+	if(!hasChildren()) return NULL;
+	if(_focus != 0) return _children[--_focus];
+	return NULL;
+}
 
+MenuItem* MenuList::getFirst(){
+	if(!hasChildren()) return NULL;
+	return _children[0];
+}
+
+MenuItem* MenuList::getPrevious(){
+	if(!hasChildren()) return NULL;
+	if(_index > 0) return _children[_index - 1];
+	return NULL;
+}
+
+MenuItem* MenuList::getNext(){
+	if(!hasChildren()) return NULL;
+	if(_index < (_childrenSize - 1)) return _children[_index + 1];
+	return NULL;
+}
+
+MenuItem* MenuList::getLast(){
+	if(!hasChildren()) return NULL;
+	return _children[_childrenSize - 1];
+}
+
+MenuItem* MenuList::setFirst(){
+	if(!hasChildren()) return NULL;
+	_index = 0;
+	return _children[_index];
+}
+
+MenuItem* MenuList::setPrevious(){
+	if(!hasChildren()) return NULL;
+	if(_index > 0) return _children[--_index];
+	return NULL;
+}
+
+MenuItem* MenuList::setNext(){
+	if(!hasChildren()) return NULL;
+	if(_index < (_childrenSize - 1)) return _children[++_index];
+	return NULL;
+}
+
+MenuItem* MenuList::setLast(){
+	if(!hasChildren()) return NULL;
+	_index = _childrenSize - 1;
+	return _children[_index];
 }
 
 bool MenuList::hasChild(MenuItem *child){
@@ -174,13 +288,21 @@ bool MenuList::hasChildren(){
 	return (bool)_childrenSize;
 }
 
+uint16_t MenuList::getSize(){
+	return _childrenSize;
+}
+
 MenuItem* MenuList::enter(){
 	if(!hasChildren()) return this;
-	_children[_focus]->caller(this);
+	for(uint16_t i = 0; i < _childrenSize; ++i){
+		_children[i]->caller(this);
+	}
+//	_children[_focus]->caller(this);
 	return _children[_focus];
 }
 
 MenuItem* MenuList::exit(){
-	if(hasParents()) return _caller;
-	return this;
+	if(!hasParents()) return this;
+	_children[_focus]->caller(NULL);
+	return _caller;
 }
--- Menu.h
+++ Menu.h
@@ -1,53 +1,25 @@
+/*
+ * This Arduino library is for managing a menu system
+ * Copyright (C) 2020  Pierre-Loup Martin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef MENU_H
 #define MENU_H
 
 #include <Arduino.h>
-
-/*
- *	Defining classes for handling menus.
- *		Needs for a menu list :			name
- *										list of children ()
- *										Active child
- *									display options :
- *										first child
- *		Basic need for a menu item :	name
- *										parent
- *										function 	(what to do when the menu is entered)
- *										item / filename / etc.
- *
- *	Notes :
- *		We need two things for each menu level :
- *			A list of items
- *				each item has :
- *					name
- *					parent (whose it's not attached, but that calls it)
- *					other data (function callback, filename, ID, etc.)					
- *			a way to managing this list as an entity.
- *				list has :
- *					name
- *					children
- *					active child
- *
- *		
- *										
- */
-/*
-struct MenuItem;
-
-struct MenuList {
-	char *name;
-	MenuItem *children;
-	MenuItem *activeChild;
-	MenuItem *firstDisplayedChild;
-};
-
-struct MenuItem{
-	char *name;
-	MenuList *parent;
-	// filename
-	// Callback function
-};
-*/
 
 class MenuItem;
 class MenuList;
@@ -56,7 +28,7 @@ class MenuEntry;
 class MenuItem{
 public:
 	MenuItem();
-	~MenuItem();
+	virtual ~MenuItem();
 
 	friend class MenuList;
 
@@ -65,45 +37,87 @@ public:
 
 	bool hasParent(MenuItem *parent);
 	bool hasParents();
+
+	bool hasFocus();
+//	MenuItem* focusItem();
+	MenuItem* focusNextItem();
+	MenuItem* focusPreviousItem();
+
+	MenuItem* getNextItem();
+	MenuItem* getPreviousItem();
+
+
 //	int16_t id();
 
 protected:
-	MenuItem* addParent(MenuItem *parent);
-	MenuItem* removeParent(MenuItem *parent);
+	MenuItem* addParent(MenuList *parent);
+	MenuItem* removeParent(MenuList *parent);
 
-	MenuItem* caller(MenuItem * item);
-	MenuItem* caller();
+	virtual inline void caller(MenuList * list){_caller = list;};
 
 private:
-
+	bool _hasFocus;
 	char *_name;
-	int16_t _parentsSize;
-	MenuItem **_parents;
-	MenuItem *_caller;
+	uint16_t _parentsSize;
+	MenuList **_parents;
+	MenuList *_caller;
 //	int16_t _id;
 };
 
 class MenuList : public MenuItem {
 public:
 	MenuList();
-	~MenuList();
+	virtual ~MenuList();
 
 	MenuItem* addChild(MenuItem *child);
 	MenuItem* removeChild(MenuItem *child);
 	MenuItem* deleteChild(MenuItem *child);
-	MenuItem* focus();
-	MenuItem* previous();
-	MenuItem* next();
+
+	MenuItem* getFocus();
+	MenuItem* focusNextItem();
+	MenuItem* focusPreviousItem();
+
+	MenuItem* getFirst();
+	MenuItem* getPrevious();
+	MenuItem* getNext();
+	MenuItem* getLast();
+
+	MenuItem* setFirst();
+	MenuItem* setPrevious();
+	MenuItem* setNext();
+	MenuItem* setLast();
 
 	bool hasChild(MenuItem *child);
 	bool hasChildren();
+	
+	uint16_t getSize();
+
 	MenuItem* enter();
 	MenuItem* exit();
 
 private:
-	int16_t _focus;
-	int16_t _childrenSize;
+	uint16_t _focus;
+	uint16_t _index;
+	uint16_t _childrenSize;
 	MenuItem **_children;
+
+};
+
+class Menu : public MenuList{
+public:
+//	Menu();
+//	~Menu();
+
+	inline void displaySize(uint16_t size){_displaySize = size;};
+	inline uint16_t displaySize(){return _displaySize;};
+
+	MenuItem* firstItem();
+	MenuItem* previousItem();
+	MenuItem* nextItem();
+	MenuItem* lastItem();
+
+private:
+	uint16_t _displaySize;
 
 };
 
